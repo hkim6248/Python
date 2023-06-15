14c510a78b6be6b644e7656a772c574e7fc3f742
hkim6248 <hkim6248@gmail.com>
2023-06-14 22:32:20 -0400

    Python_2023_6_14_Lecture_1_Quiz_1

---
+++ Python_2023_6_14_Lecture_1_Quiz_1.py
@@ -0,0 +1,46 @@
+print("Hello!")
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1)) #12
+print('Balloon')
+print("Balloon")
+print("X")
+print("X"*10)
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10)) #True
+
+animal = "dog"
+name = "Yeontan"
+age = 4
+hobby = "taking a walk"
+is_a_baby = age <= 3
+
+print("My " + animal + "'s name is " + name + ".")
+print("He is " + str(age) + " years old.")
+
+hobby = "taking a bath"
+
+print("His hobby is " + hobby + ".")
+print("Is he a baby dog? " + str(is_a_baby) + ".")
+
+'''Test_1
+Test_2
+Test_3'''
+
+print('''Test_1
+Test_2
+Test_3''')
+
+#Quiz 1
+station = "Bethesda"
+
+print("This station is " + station + ".")
